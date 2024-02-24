44b66b87fb91f2dbb9e82ad3e35f28e6d5df14f5
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-02-24 15:22:15 -0300

    feat(Sorting): add insertion_sort

---
+++ Sorting/insertion_sort.py
@@ -0,0 +1,25 @@
+from random import shuffle
+
+def insertion_sort(arr):
+    length = len(arr)
+    for i in range(1, length):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key;
+    return arr
+
+
+if __name__ == "__main__":
+    order = list(range(10))
+    reverse = list(range(9, -1, -1))
+    random = list(range(10))
+    shuffle(random)
+    insertion_sort(order)
+    insertion_sort(reverse)
+    insertion_sort(random)
+    print('order', order)
+    print('reverse', reverse)
+    print('random', random)
