205e727589ee839aa8a0b41bfb6cfb2299427365
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-02-24 09:33:55 -0300

    test: add pytest library and parametrize tests

--- Sorting/test.py
+++ Sorting/test.py
@@ -1,29 +1,23 @@
-import unittest
+import pytest
 import random
 from bubble_sort import bubble_sort
 from selection_sort import selection_sort
 
+@pytest.mark.parametrize("algorithm", [(bubble_sort),(selection_sort)])
+def test_sorting_random_numbers(algorithm):
+    random_numbers = random.sample(range(100), 100)
+    result = algorithm(random_numbers)
+    assert result == list(range(100))
 
-class TestSortingAlgorithms(unittest.TestCase):
-    # Uncomment for test Algorithm
-    ALG = []
-    ALG.append(bubble_sort)
-    # ALG.append(selection_sort)
+@pytest.mark.parametrize("algorithm", [(bubble_sort), (selection_sort)])
+def test_sorting_reverse_numbers(algorithm):
+    reverse_numbers = list(range(99, -1, -1))
+    result = algorithm(reverse_numbers)
+    assert result == list(range(100))
 
-    def test_sorting_random_numbers(self):
-        random_numbers = random.sample(range(100), 100)
-        result = self.ALG[0](random_numbers)
-        self.assertEqual(result, list(range(100)))
+@pytest.mark.parametrize("algorithm", [(bubble_sort), (selection_sort)])
+def test_sorting_order_numbers(algorithm):
+    order_numbers = list(range(100))
+    result = algorithm(order_numbers)
+    assert result == order_numbers
 
-    def test_sorting_reverse_numbers(self):
-        reverse_numbers = list(range(99, -1, -1))
-        result = self.ALG[0](reverse_numbers)
-        self.assertEqual(result, list(range(100)))
-
-    def test_sorting_order_numbers(self):
-        order_numbers = list(range(100))
-        result = self.ALG[0](order_numbers)
-        self.assertEqual(result, order_numbers)
-
-if __name__ == '__main__':
-    unittest.main()
