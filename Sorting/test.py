d39abc520660f2885d526c98f0b233fadc9265dc
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-02-25 12:06:07 -0300

    test(Sorting): add quick_sort in test

--- Sorting/test.py
+++ Sorting/test.py
@@ -1,25 +1,25 @@
 import pytest
 import random
 from bubble_sort import bubble_sort
-from quick_sort import quick_sort
 from selection_sort import selection_sort
 from insertion_sort import insertion_sort
 from shell_sort import shell_sort
 from merge_sort import merge_sort
+from quick_sort import quick_sort
 
-@pytest.mark.parametrize("algorithm", [(bubble_sort),(selection_sort),(insertion_sort),(shell_sort),(merge_sort)])
+@pytest.mark.parametrize("algorithm", [(bubble_sort),(selection_sort),(insertion_sort),(shell_sort),(merge_sort),(quick_sort)])
 def test_sorting_random_numbers(algorithm):
     random_numbers = random.sample(range(100), 100)
     result = algorithm(random_numbers)
     assert result == list(range(100))
 
-@pytest.mark.parametrize("algorithm", [(bubble_sort), (selection_sort),(insertion_sort),(shell_sort),(merge_sort)])
+@pytest.mark.parametrize("algorithm", [(bubble_sort), (selection_sort),(insertion_sort),(shell_sort),(merge_sort),(quick_sort)])
 def test_sorting_reverse_numbers(algorithm):
     reverse_numbers = list(range(99, -1, -1))
     result = algorithm(reverse_numbers)
     assert result == list(range(100))
 
-@pytest.mark.parametrize("algorithm", [(bubble_sort), (selection_sort),(insertion_sort),(shell_sort),(merge_sort)])
+@pytest.mark.parametrize("algorithm", [(bubble_sort), (selection_sort),(insertion_sort),(shell_sort),(merge_sort),(quick_sort)])
 def test_sorting_order_numbers(algorithm):
     order_numbers = list(range(100))
     result = algorithm(order_numbers)
