3b284325d29b920911c1b104e9807ac7c91a1258
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-02-25 10:08:55 -0300

    test(Sorting): add merge_sort in test

--- Sorting/test.py
+++ Sorting/test.py
@@ -4,20 +4,23 @@ from bubble_sort import bubble_sort
 from selection_sort import selection_sort
 from insertion_sort import insertion_sort
 from shell_sort import shell_sort
+from merge_sort import merge_sort
 
-@pytest.mark.parametrize("algorithm", [(bubble_sort),(selection_sort),(insertion_sort),(shell_sort)])
+@pytest.mark.parametrize("algorithm", [(bubble_sort),(selection_sort),(insertion_sort),(shell_sort),(merge_sort)])
 def test_sorting_random_numbers(algorithm):
     random_numbers = random.sample(range(100), 100)
     result = algorithm(random_numbers)
     assert result == list(range(100))
+    from merge_sort import merge_sort
 
-@pytest.mark.parametrize("algorithm", [(bubble_sort), (selection_sort),(insertion_sort),(shell_sort)])
+@pytest.mark.parametrize("algorithm", [(bubble_sort), (selection_sort),(insertion_sort),(shell_sort),(merge_sort)])
 def test_sorting_reverse_numbers(algorithm):
     reverse_numbers = list(range(99, -1, -1))
     result = algorithm(reverse_numbers)
     assert result == list(range(100))
+    from merge_sort import merge_sort
 
-@pytest.mark.parametrize("algorithm", [(bubble_sort), (selection_sort),(insertion_sort),(shell_sort)])
+@pytest.mark.parametrize("algorithm", [(bubble_sort), (selection_sort),(insertion_sort),(shell_sort),(merge_sort)])
 def test_sorting_order_numbers(algorithm):
     order_numbers = list(range(100))
     result = algorithm(order_numbers)
