907f85eadf9dc2e789a25306614e9760daf51f99
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-02-28 13:59:50 -0300

    perf(Sorting/insertion): remove key variable

--- Sorting/insertion_sort.py
+++ Sorting/insertion_sort.py
@@ -3,14 +3,11 @@ from random import shuffle
 def insertion_sort(arr):
     length = len(arr)
     for i in range(1, length):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
+        j = i
+        while j > 0 and arr[j] < arr[j-1]:
+            arr[j], arr[j-1] = arr[j-1], arr[j]
             j -= 1
-        arr[j + 1] = key;
     return arr
-
 
 if __name__ == "__main__":
     order = list(range(10))
