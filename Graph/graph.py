5cf397762e94358d184430f927db9f590b1604e6
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-03-11 14:40:25 -0300

    feat(Graph): add graph structure

---
+++ Graph/graph.py
@@ -0,0 +1,19 @@
+# Graph using dictionary and Sets
+class Graph:
+    def __init__(self):
+        self.graph = {}
+    
+    def insert_node(self, node):
+        pass
+
+    def delete_node(self):
+        pass
+
+    def search_node(self):
+        pass
+
+    def traverse(self):
+        pass
+
+if __name__ == "__main__":
+    graph = Graph()
